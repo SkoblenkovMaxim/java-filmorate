47588fd9480d9abb36383f457aaeb3328f7fe91d
gorbulenkogv <gorbulenko.grigoriy@yandex.ru>
2024-10-30 20:42:05 +0300

    Поправил рекомендации

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.service.user;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,16 +8,24 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.director.Director;
 import ru.yandex.practicum.filmorate.model.event.EventOperation;
+import ru.yandex.practicum.filmorate.model.film.Film;
 import ru.yandex.practicum.filmorate.model.film.FilmDto;
 import ru.yandex.practicum.filmorate.model.film.FilmMapper;
 import ru.yandex.practicum.filmorate.model.friend.Friends;
+import ru.yandex.practicum.filmorate.model.genre.Genre;
+import ru.yandex.practicum.filmorate.model.rating.Rating;
 import ru.yandex.practicum.filmorate.model.user.User;
 import ru.yandex.practicum.filmorate.model.user.UserDto;
 import ru.yandex.practicum.filmorate.model.user.UserMapper;
 import ru.yandex.practicum.filmorate.service.event.EventService;
+import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.friend.FriendStorage;
+import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
+import ru.yandex.practicum.filmorate.storage.like.LikeStorage;
+import ru.yandex.practicum.filmorate.storage.raiting.RatingStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import static java.lang.String.format;
@@ -31,18 +37,30 @@ public class UserService {
     private final UserStorage userStorage;
     private final FriendStorage friendStorage;
     private final FilmStorage filmStorage;
+    private final LikeStorage likeStorage;
+    private final GenreStorage genreStorage;
+    private final RatingStorage ratingStorage;
+    private final DirectorStorage directorStorage;
     private final UserMapper userMapper;
     private final FilmMapper filmMapper;
     private final EventService eventService;
 
     public UserService(@Qualifier("userDbStorage") UserStorage userStorage,
                        @Qualifier("filmDbStorage") FilmStorage filmStorage,
+                       @Qualifier("likeDbStorage") LikeStorage likeStorage,
+                       @Qualifier("genreDbStorage") GenreStorage genreStorage,
+                       @Qualifier("ratingDbStorage") RatingStorage ratingStorage,
+                       @Qualifier("directorDbStorage") DirectorStorage directorStorage,
                        FriendStorage friendStorage,
                        UserMapper userMapper,
                        FilmMapper filmMapper, EventService eventService) {
         this.userStorage = userStorage;
         this.filmStorage = filmStorage;
+        this.likeStorage = likeStorage;
         this.friendStorage = friendStorage;
+        this.genreStorage = genreStorage;
+        this.ratingStorage = ratingStorage;
+        this.directorStorage = directorStorage;
         this.userMapper = userMapper;
         this.filmMapper = filmMapper;
         this.eventService = eventService;
@@ -159,11 +177,14 @@ public class UserService {
         List<Long> userFilms = filmStorage.getFilmsLikesByUser(userId);
         log.info("Список фильмов, которые лайкнул пользователь {}", userId);
         recommendUserFilms.removeAll(userFilms);
-        return recommendUserFilms
+        List<Film> films = recommendUserFilms
                 .stream()
                 .map(filmStorage::getFilm)
-                .map(filmMapper::toFilmDto)
                 .toList();
+        films.forEach(this::fillFilmAdditionalInfo);
+        return films.stream()
+                .map(filmMapper::toFilmDto)
+                .collect(Collectors.toList());
     }
 
     //Проверка наличия пользователя в хранилище
@@ -203,5 +224,29 @@ public class UserService {
     }
     private String getDisplayedName(User user) {
         return user.getName() != null && !user.getName().isBlank() ? user.getName() : user.getLogin();
+    }
+
+    private void fillFilmAdditionalInfo(Film film) {
+        Rating rating = ratingStorage.getRatingById(film.getMpa().getId());
+        if (rating != null) {
+            film.getMpa().setName(rating.getName());
+            film.getMpa().setDescription(rating.getDescription());
+        }
+
+        List<Genre> genreList = new ArrayList<>();
+        genreStorage.getFilmGenresByFilmId(film.getId())
+                .forEach(filmGenre -> {
+                    Genre genre = genreStorage.getGenreById(filmGenre.getGenreId());
+                    if (genre != null) {
+                        genreList.add(genre);
+                    }
+                });
+        genreList.sort(Comparator.comparing(Genre::getId));
+        film.setGenres(new HashSet<>(genreList).stream().toList());
+
+        List<Director> directors = directorStorage.getDirectorsByFilmId(film.getId());
+        film.setDirectors(directors);
+
+        film.setLikeCount(likeStorage.getLikesCount(film.getId()));
     }
 }
